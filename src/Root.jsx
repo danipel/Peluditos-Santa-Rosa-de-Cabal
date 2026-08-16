d576925be23021f74f60616a72c2731708242eda
Daniel Pelaez Chica <d.pelaez@utp.edu.co>
2026-08-15 19:14:19 -0500

    Scroll to top

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./home";
 import App from "./App"
+import ScrollToTop from "./components/layout/ScrollToTop";
 
 export default function Root() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/" element={<App />} />
